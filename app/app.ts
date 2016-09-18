05e94267211562aa2db07d4de28f3a69b33462ec
Ben Shope <nimajnebs@gmail.com>
2016-09-17 18:38:49 -0700

    Add homepage lines

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
+import {HomePage} from './pages/home/home';
+
 
 
 @Component({
